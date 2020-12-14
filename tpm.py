fe6a216716566256478868bcd26db1ccf9cf9fe3
Hasan Kashi <61319182+hasan-kashi@users.noreply.github.com>
2020-12-14 15:19:35 +0330

    Create tpm.py

---
+++ tpm.py
@@ -0,0 +1,14 @@
+from urllib.request import URLopener
+from sys import argv
+from os import mkdir
+from os.path import isdir
+
+if not isdir('Packages'):
+	mkdir('Packages')
+
+if argv[1] == 'install':
+	pkg = argv[2]
+	try:
+		URLopener().retrieve(f'https://raw.githubusercontent.com/MajArtsGroup/Tiny-Package-Manager/main/pack/{pkg}.h', f'Packages/{lib}.h')
+	except:
+		print(f"Package {pkg} not found")
